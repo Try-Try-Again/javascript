3f91ef679a932800c4e28211e1fee8137d2d44d2
Your Name <you@example.com>
2019-10-13 19:11:46 -0700

    moving to pc

--- eloquent_javascript/ch05/characters.js
+++ eloquent_javascript/ch05/characters.js
@@ -7,18 +7,37 @@ function countBy (items, groupName) {
     // create a var called group with the result of running the second argument
     // with the item as an argument
     let name = groupName(item);
-    // create a var called known
-    // assign it the result of the following...
-      // still dont know what the fuck this is aboue :/
+    // create a var called known and assign it the result of the location
+    // of any item inside the array (-1 for none).
     let known = counts.findIndex(c => c.name == name);
+    //...
     if (known == -1) {
+      // add an element to counts with an object containing the items name and count
       counts.push({name, count: 1});
     } else {
+      // otherwise increment the 'known' attribute by  1
       counts[known].count++;
     }
   }
   return counts;
 }
 
-console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+//console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
 // [{name: false, count: 2}, {name: true, count: 3}]
+
+function textScripts(text) {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.name : "none";
+  }).filter(({name}) => name != "none");
+
+  let total = scripts.reduce((n, {count}) => n + count, 0);
+  if (total == 0) return "No scripts found";
+
+  return scripts.map(({name, count}) => {
+    return `${Math.round(count * 100 / total)}% ${name}`;
+  }).join(", ");
+}
+
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+// 61% Han, 22% Latin, 17% Cyrillic
--- eloquent_javascript/ch05/manuscripts.js
+++ eloquent_javascript/ch05/manuscripts.js
@@ -96,5 +96,58 @@ function characterScript(code) {
   return null;
 }
 
-console.log(characterScript(121));
+//console.log(characterScript(121));
 // {name: "Latin", ...}
+
+
+// create a function called countBy that takes args for items and group name
+function countBy (items, groupName) {
+  // create an array called counts
+  let counts = [];
+  // for every item in items (must be an array)...
+  for (let item of items) {
+    // create a var called group with the result of running the second argument
+    // with the item as an argument
+    let name = groupName(item);
+    // create a var called known and assign it the result of the location
+    // of any item inside the array (-1 for none).
+    let known = counts.findIndex(c => c.name == name);
+    //...
+    if (known == -1) {
+      // add an element to counts with an object containing the items name and count
+      counts.push({name, count: 1});
+    } else {
+      // otherwise increment the 'known' attribute by  1
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+//console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+// [{name: false, count: 2}, {name: true, count: 3}]
+
+// function takes in a string as an arg
+function textScripts(text) {
+  // get an array of scripts and their char count from 'text'
+  let scripts = countBy(text, char => {
+    // assign the character code from the character to 'script'
+    let script = characterScript(char.codePointAt(0));
+    //...
+    return script ? script.name : "none";
+    // filter out all the items that are 'none'
+  }).filter(({name}) => name != "none");
+
+  // use reduce to add up all the elements in array and up the count attributes
+  let total = scripts.reduce((n, {count}) => n + count, 0);
+  //...
+  if (total == 0) return "No scripts found";
+
+  // map the name and count of each script to a human readble string
+  return scripts.map(({name, count}) => {
+    return `${Math.round(count * 100 / total)}% ${name}`;
+  }).join(", ");
+}
+
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+// 61% Han, 22% Latin, 17% Cyrillic
